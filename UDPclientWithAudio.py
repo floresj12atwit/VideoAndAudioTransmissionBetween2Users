5d510317b4ec986853112dc317ae63c29ef1c4ea
Floresj12atwit <floresj12@wit.edu>
2023-12-08 00:32:15 -0500

    Commented server and client codes

--- UDPclientWithAudio.py
+++ UDPclientWithAudio.py
@@ -8,22 +8,31 @@ BUFF_SIZE= 65536
      
 def runClient():  #IP will most likely need to be passed in
     BREAK = False
+
+    #Global variable for the client socket
     global client_socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
     
+    #Get the host name and set the initial host IP
     host_name = socket.gethostname()
     host_ip = '127.0.0.1' # This will need to be dynamically updated somehow use the IP made in the server
     print(host_ip)
     port = 4444
+
+    #Message to initiate communiation between sockets
     message = b'TAKE THIS! :D'
     
     
-    
+    #Import ThreadPoolExecutor for concurrent exectution
     from concurrent.futures import ThreadPoolExecutor
+
+    #Use the ThreadPoolExecutor to concurrently execute audio and video streams
     with ThreadPoolExecutor(max_workers=2) as executor:
         #executor.submit(video_stream, client_socket)
+        #Submit the audio_stream function to the executor
         executor.submit(audio_stream, host_ip, port, BREAK)
+        #Submit the video_stream function with the initial message to the executor
         executor.submit(video_stream, message, host_ip, port)
         
         
@@ -33,37 +42,54 @@ def video_stream(message, host_ip, port):
         
         #print('Entered video_stream function')
         
+        #Create a window for displaying the received video
         cv2.namedWindow('RECEIVING VIDEO')        
         cv2.moveWindow('RECEIVING VIDEO', 10,360)
+
+        #Variables for tracking frames per second (FPS)
         fps,st,frames_to_count,cnt = (0,0,20,0)
         
 
         try:
+                #Send the initial message to start video streaming
                 client_socket.sendto(message, (host_ip, port))
-                while True:
+                while True:       #Main loop for receiving and displaying vidoe frames
                         
-                        packet,_ = client_socket.recvfrom(BUFF_SIZE)
+                        packet,_ = client_socket.recvfrom(BUFF_SIZE)     #Receive a packet from the client socket
                         
-                        decoded_packet = packet.decode("utf-8")
+                        decoded_packet = packet.decode("utf-8")          #Decode the received packet
                         #print(decoded_packet)
+
+                        #Check if received packet indicated the end of the video stream
                         if decoded_packet == "VideoEnd":
                                #print("HERE")
+                               #Confirmed the end of the video stream to the client
                                message = b"VideoEndConfirm"
                                client_socket.sendto(message, (host_ip, port))
+
+                               #Close the client socket
                                client_socket.close()
                                print("client socket closed")
+
+                        #Decode the base64-encoded data and convert it to a NumPy array
                         data = base64.b64decode(packet,' /')
                         npdata = np.fromstring(data, dtype = np.uint8)
-
+                        
+                        #Decode the NumPy array to obtain the video frame 
                         frame = cv2.imdecode(npdata,1)
+                        
+                        #Display the video frame with FPS information
                         frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
                         cv2.imshow("RECEIVING VIDEO",frame)	        
+                        
+                        
+                        #Check for user input to exit the loop
                         key = cv2.waitKey(1) & 0xFF
                 
                         if key == ord('q'):
                                 client_socket.close()
-                                #os._exit(1)
                                 break
+                        #Update the FPS information
                         if cnt == frames_to_count:
                                 try:
                                         fps = round(frames_to_count/(time.time()-st))
@@ -76,51 +102,74 @@ def video_stream(message, host_ip, port):
         except ConnectionResetError:
                 print("Connection was forcibly closed by the remote host.")
         finally:
+                #Close the client socket 
                 client_socket.close()
                 
                 #cv2.destroyWindow()
                 
 
 def audio_stream(host_ip, port, BREAK):
-       
+        #Initalize PyAudio object for audio processing
         p = pyaudio.PyAudio()
-        CHUNK = 1024
+
+        #Initalize Chunk size same as server code
+        CHUNK = 1024  
+        #Set up audio stream parameters (similar to how it's done in the server code)      
         stream = p.open(format=p.get_format_from_width(2),
 					channels=2,
 					rate=44100,
 					output=True,
 					frames_per_buffer=CHUNK)
-        #create socket for audio stream (TCP)
+        
+        #Create a TCP socket for audio stream
         client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_address1 = (host_ip, port-1)      #chooses the next port over for the audio stream 
+        '''
+        This is the "handshake" for the TCP connection
+        '''
         print('server listening at',socket_address1)
         client_socket1.connect(socket_address1) 
         print("CLIENT CONNECTED TO",socket_address1)
+
+        #Initalize variables for receiving audio data
         data = b""
-        payload_size = struct.calcsize("Q")
-        #packet = client_socket1.recv(BUFF_SIZE)
+        payload_size = struct.calcsize("Q")            #Payload is the actual data being transmitted
         print(data)
+
+
         while True:
                try:
                         
+                        #Receive audio data packets
                         while len(data) < payload_size:
+                               #Receive packets until there is enough data for the payload size
                                packet = client_socket1.recv(4*1024)
-                               if not packet: break
+                               if not packet: break     #Break loop if no packet is received
                                data += packet
+                        
+                        #Extract the packed payload size from the received data 
                         packed_msg_size = data[:payload_size]
                         data = data[payload_size:]
                         msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+                        #Receive audio data until there is enough data for the complte payload
                         while len(data) < msg_size:
                                data += client_socket1.recv(4*1024)
+
+                        #Extract the frame data (payload) from the received data 
                         frame_data = data[:msg_size]
                         data = data[msg_size:]
+
+                        #Deserialize the frame data and play the audio
                         frame = pickle.loads(frame_data)
+                        #Play the audio frame
                         stream.write(frame)
                except:
-                
+
                       break
+        #Close the client socket after each loop
         client_socket1.close()
-        print('Audio closed Client', BREAK)
+        print('Audio closed Client')
         
 
-#runClient()
+runClient()      
--- UDPserverWithAudio.py
+++ UDPserverWithAudio.py
@@ -1,7 +1,3 @@
-#This is the UDP server that the first user to enter a room is going to create 
-#with the purpose of video streaming, this design may have to be changed since the goal is have 
-#all users be able to manipulare the video 
-
 import cv2, imutils, socket
 import numpy as np
 import time
@@ -12,50 +8,64 @@ import os
 import sys
 
 
-video_path = 'WatchParty/website/Videos/video.mp4'    #this will be dynamically updated when we download yotube videos
-audio_path = 'WatchParty/website/Videos/videoAudio.wav'
+video_path = 'VideoBetween2Users/Videos/video.mp4'    
+audio_path = 'VideoBetween2Users/Videos/videoAudio.wav'
 
 
 #The plan is to launch a UDP server when a user enters a video
 #And then all users will connect to it and share a video stream
 BUFFER_SIZE= 65536
-def runVideoServer(local_video_path, local_audio_path):   #this needs to pass be passed in the users IP  
+def runVideoServer(local_video_path, local_audio_path):   #this needs to pass be passed in the users IP if it is going to be deployed to a real web server  
     
-    #command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(local_video_path, audio_file)
-    #os.system(command)
-    global server_socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    global server_socket                  #server socket is made global so that video generation function can call it 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        #Set up a UDP socket for video transmission between users 
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFFER_SIZE)
 
-    print("I have arrived")
+    print("Server initalization complete")
     
-    #For Listening at we may need to import the user that's calling its IP
+
     host_name = socket.gethostname()
-    host_ip = '127.0.0.1'  #This will need to be updated to reflect an actual IP  
+    host_ip = '127.0.0.1'  #This will need to be updated to reflect an actual IP if the website is deployed
     print(host_ip)
 
-    port = 4444
+    #Set up server socket with the specified IP and port
+    port = 4444         #Arbitrary port number we chose 4444
     socket_address = (host_ip, port)  #this is the IP and address and port needed for address
-    server_socket.bind(socket_address)
+    server_socket.bind(socket_address)      #Binds the socket according to the server IP and port
     print("Listening at", socket_address)
 
+    #Set up a global queue for storing video frames
     global q 
     q = queue.Queue(maxsize=10)             #this defines the maximum size of the queue
+    
+    #Sets up a global video capture object for reading frames from a local video file
     global vid
     vid = cv2.VideoCapture(local_video_path)
-    FPS = vid.get(cv2.CAP_PROP_FPS)
     
-    global videoEnd
-    videoEnd=False
-    global TS
-    TS = .5/FPS
-    global BREAK
+    
+    FPS = vid.get(cv2.CAP_PROP_FPS)     #Get the native FPS of the downloaded video
+    print(FPS)
+
+    global TS                           #Set up a global variable for controlling the transmission speed
+    TS = .5/FPS                         #Initial transmission speed
+    
+    global BREAK                        #Initalizes a global BREAK variable (not used)
     BREAK = False
+
+    #Print information about the downloaded video
     print('FPS: ', FPS, TS)
     totalNumFrames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
     durationInSeconds = float(totalNumFrames)/float(FPS)
     d=vid.get(cv2.CAP_PROP_POS_MSEC)    #this function gets the current position in the video file could be used for playback feature
     print(durationInSeconds, d)
+
+
+    '''
+    This does not provide true parallelism but it allows us to execute these tasks at the same time to have the video and audio transmit to the client
+    at the same time
+    No deadlocks have been encountered in all the test we've ran so it is "safe" but proper deadlock handling is not implemented as we did not encounter it as an issue
+    '''
     from concurrent.futures import ThreadPoolExecutor
     with ThreadPoolExecutor(max_workers=3) as executor:         #arguments must be passed in since we run this file from another function and not just the file itself
      
@@ -64,42 +74,59 @@ def runVideoServer(local_video_path, local_audio_path):   #this needs to pass be
      executor.submit(video_stream, q, FPS)
      
      
-
+'''
+This handles the video stream (transmitting video from server to client)
+'''
 def video_stream(q, FPS):
-     global TS
-     fps,st,frames_to_count,cnt = (0,0,1,0)
-     cv2.namedWindow('Transmitting Video')
-     cv2.moveWindow('Transmitting Video', 10, 30)
+     global TS          #Global variable to control the transmission speed of the frames
+     fps,st,frames_to_count,cnt = (0,0,1,0)        #Initalize variable for tracking frames per second (FPS)
+     
+     
+     cv2.namedWindow('Transmitting Video')          #Create a video for displaying the video
+     cv2.moveWindow('Transmitting Video', 10, 30)   #Move the pop up window so that it can be seen alongside receiving window
 
-     while True:
+     while True:        #Main loop for video streaming
           
-          msg, client_address = server_socket.recvfrom(BUFFER_SIZE)
+          msg, client_address = server_socket.recvfrom(BUFFER_SIZE)     #Receive a message and client address from the server socket
           print('GOT Connection from ', client_address)
-          WIDTH = 400
+          
 
           while(True):
             #print(TS)
             #print(fps)
-            frame= q.get()
+            frame= q.get()          #Get the current video frame for the queue
             #print(frame)
-            if frame is "VideoEnd":
+
+
+            if frame is "VideoEnd":         #Check if received frame indicated the end of the video stream
                 print(frame, "WE ARE HERE")
                 time.sleep(3)
+                #Send a message indicating the end of the video stream to the client
                 message = b"VideoEnd"
                 server_socket.sendto(message,client_address)
-                
+
+
+            #Encode the frame and prepare it for transmission 
             encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
             message = base64.b64encode(buffer)
+
+
+            #Send the encoded frame to the client
             server_socket.sendto(message,client_address)
             
             
-            
+            #Display FPS information on the video frame (the red FPS counter)
             frame = cv2.putText(frame,'FPS: '+str(round(fps,1)),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+            
+            
+            #Update the FPS and transmission speed based on the actual FPS of the video
             if cnt == frames_to_count:
                 try:
                     fps = (frames_to_count/(time.time()-st))
                     st = time.time()
                     cnt = 0
+
+                    #Adjust transmission spsed (TS) based on the actual FPS compared to the target FPS
                     if fps>FPS:
                         TS+=0.001
                     elif fps<FPS:
@@ -110,10 +137,14 @@ def video_stream(q, FPS):
                     pass
             cnt+=1
 
+            #Display the video frame 
             cv2.imshow('Transmitting Video', frame)
+
+            #Wait for a keyboard input event and check if 'q' is pressed to exit
+            #This is needed to get the frame to show even if we don't have a use for it other than exiting the video
             key = cv2.waitKey(int(1000*TS)) & 0xFF
-            if key == ord('q'):
-                #os._exit(1)
+            if key == ord('q'):     #Exit the loop is 'q' is pressed This only closes for the window it does not stop the audio stream
+                
                 TS = False
                 break
          
@@ -121,97 +152,117 @@ def video_stream(q, FPS):
      
     
 
-#This will handle the audio stream   
-#This will generate the stream of frames in queue
+'''
+This generates the stream of frames from the downloade video and put them in a queue
+'''
 def video_stream_gen(vid, BREAK):
         
-    WIDTH = 400
-    while(vid.isOpened()):
+    WIDTH = 400                 #Sets the width for the video frame (400 is used so that 1 datagram can be used for 1 frame if its bigger we need more packets)
+    while(vid.isOpened()):      #Opens the video and loops while the video is open
         try:
-            _,frame = vid.read()
+
+            _,frame = vid.read()        #Read a frame from the video and resize it
             frame = imutils.resize(frame,width = WIDTH)
 
-            q.put(frame)
+            q.put(frame)            #Put the frame into the queue that stores the frames to be displayed
         except:
-            print("video has ended server") 
-            q.put("VideoEnd")
-            time.sleep(2)
-            break
-            #os._exit(1)
+            print("video has ended server")     #Prints when the video has stopped being read 
+            q.put("VideoEnd")                   #Send a signal to the queue that the video is over a packet with the message (video end)
+            time.sleep(2)                       #Delay so that the client has time to read that the video is done
+            break               #Break out of loop
+            
     
+
+
+    #The video has ended now it will wait for a message from the client to confirm that they've received the video over message
     msg,_ = server_socket.recvfrom(BUFFER_SIZE)
     decoded_message = msg.decode("utf-8")
     print(decoded_message)
-    if decoded_message == "VideoEndConfirm":                #THis is how we ensured a graceful disconnection of the sockets 
+
+    #Checks if client sent a "VideoEndConfirm" message
+    if decoded_message == "VideoEndConfirm":                #This is how we ensured a graceful disconnection of the sockets 
         print('Player closed Server')
         BREAK=True
-        cv2.destroyAllWindows()
-        server_socket.close()
+        cv2.destroyAllWindows()         #Close the OpenCV windows (popups)
+        server_socket.close()           #Close the server socket
         print("server video socket closed")
-        vid.release()
+        vid.release()                   #Release the video capture object 
     
 
 
 
-#this will handle the audio
+'''
+This handles the audio stream (transmitting video to client)
+'''
 def audio_stream(host_ip, port, audio_file):
-    print("enter audio stream")
+    print("enter audio stream")         #Prints a message to indicate audio stream function has been entered
+    
+    #Create a new socket for audio streaming
     s = socket.socket()
-    s.bind((host_ip, (port-1)))
+    s.bind((host_ip, (port-1)))         #The next port over is used in this case "4443"
 
-    s.listen(5)         #this will listen to the connection from a the client 
-    CHUNK = 1024
-    wf = wave.open(audio_file, 'rb')
+    s.listen(5)         #this will listen to the connection from the client 
+    CHUNK = 1024        #Define the chunk size for reading audio frames (Chunk is a small piece or segment of audio data)
+    
+    
+    wf = wave.open(audio_file, 'rb')       #Open the audio file for reading
     print(wf.getnchannels())
-    p = pyaudio.PyAudio()
-    print('server listening at for audio',(host_ip, (port-1)))        
+
+
+    p = pyaudio.PyAudio()           #intialize a PyAudio instance for audio stream handling
+    print('server listening at for audio',(host_ip, (port-1)))         #Print the server's addres for audio stremaing
+    
+    #Configure the audio stream parameters audio format, channel, sample rate etc 
     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                     channels=1,
                     rate=wf.getframerate(),
                     input=True,
                     frames_per_buffer=CHUNK)
 
+    #Accept the connection from the client 
     client_socket,addr = s.accept()
     
+    #Get the total number of frames and initialize frame counter
     total_frames = wf.getnframes()
     current_frame = 0
     
     try:
+        #This Loops until the video is over (current frame exceeds total frames)
         while current_frame<total_frames:
             
-                data = wf.readframes(CHUNK)
-                a = pickle.dumps(data)
-                message = struct.pack("Q",len(a))+a
+                data = wf.readframes(CHUNK)   #Reads audio frames from the file
+
+                a = pickle.dumps(data)        #Turning the data into a byte stream
+                message = struct.pack("Q",len(a))+a #Pack the serialized data along with its length
                 
-                client_socket.sendall(message)
-                current_frame += CHUNK
+                client_socket.sendall(message)  #Send the packed message to the client
+                current_frame += CHUNK          #Update the current frame counter by Chunk (1024)
                 #print("audio stream is done")
 
     except Exception as e:
+        #Handle error if audio stream fails
         print("Error in audio stream:", str(e))
     finally:
         # Close resources in the finally block
         wf.close()
-        print("Audio stream is done")
+        
+        print("Audio stream is done2")
+
+        #CLose the client socket
         client_socket.close()
+
+        #Send the end message to the client
         end_message = b"AudioEnd"
         client_socket.sendall(end_message)
 
+        #Stop and close the audio stream
         stream.stop_stream()
         stream.close()
+
+        #Terminate PyAudio object
         p.terminate()
-
-    print("Audio stream is done")
-    # Send a signal or message to indicate the end of the audio stream
-    end_message = b"AudioEnd"
-    client_socket.sendall(end_message)   
-
-def stopAudio():
-    msg, client_address = server_socket.recvfrom(BUFFER_SIZE)
-    print(msg, "in handle exit")
-    if msg == "VideoEndConfirm":
-        print('video is DONE')
-    pass
+ 
 
 
-#runVideoServer(video_path, audio_path)
+
+runVideoServer(video_path, audio_path) 
